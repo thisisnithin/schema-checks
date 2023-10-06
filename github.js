0282e4461e59bb82a58c94629b25d7f4c2a4c062
thisisnithin <nithinkumar5353@gmail.com>
2023-10-06 17:16:18 +0530

    update

--- github.js
+++ github.js
@@ -42,6 +42,7 @@ const getLatestCommitSHA = () => {
     const commitSHA = execSync("git rev-parse HEAD").toString().trim();
     return commitSHA;
   } catch (err) {
+    console.log(err);
     return;
   }
 };
