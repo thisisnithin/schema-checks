214d1d07166832f32e255a932b27745fafe5c349
thisisnithin <nithinkumar5353@gmail.com>
2023-10-06 17:49:43 +0530

    update

--- github.js
+++ github.js
@@ -1,51 +1,12 @@
-const { readFileSync } = require("node:fs");
 const { execSync } = require("child_process");
 
 // https://docs.github.com/en/actions/learn-github-actions/environment-variables#default-environment-variables
 
-function parseBranch(branch) {
-  return branch
-    ? /^(?:refs\/heads\/)?(?<branch>.+)$/i.exec(branch)?.[1]
-    : undefined;
-}
-
-const getPrEvent = () => {
-  try {
-    const event = process.env.GITHUB_EVENT_PATH
-      ? JSON.parse(readFileSync(process.env.GITHUB_EVENT_PATH, "utf8"))
-      : undefined;
-
-    if (event && event.pull_request) {
-      return {
-        branch: event.pull_request.base
-          ? parseBranch(event.pull_request.base.ref)
-          : undefined,
-        pr: event.pull_request.number,
-      };
-    }
-  } catch {
-    // Noop
-  }
-
-  return { pr: undefined, branch: undefined };
-};
-
-const getPrNumber = () => {
-  const event = process.env.GITHUB_EVENT_PATH
-    ? JSON.parse(readFileSync(process.env.GITHUB_EVENT_PATH, "utf8"))
-    : undefined;
-
-  return event && event.pull_request ? event.pull_request.number : undefined;
-};
-
 function getLatestPRCommit() {
   try {
-    const branchName = execSync("git rev-parse --abbrev-ref HEAD")
-      .toString()
-      .trim();
-
-    console.log({ branchName });
-    return execSync(`git log ${branchName} -1 --pretty=format:"%H"`).toString();
+    return execSync(
+      `git log ${process.env.GITHUB_HEAD_REF} -1 --pretty=format:"%H"`
+    ).toString();
   } catch (error) {
     return undefined;
   }
@@ -55,24 +16,18 @@ function useGitHub() {
   const isPr =
     process.env.GITHUB_EVENT_NAME === "pull_request" ||
     process.env.GITHUB_EVENT_NAME === "pull_request_target";
-  const branch = parseBranch(
-    process.env.GITHUB_EVENT_NAME === "pull_request_target"
-      ? `refs/pull/${getPrNumber()}/merge`
-      : process.env.GITHUB_REF
-  );
+
+  const commit = getLatestPRCommit();
 
   return {
-    ref: process.env.GITHUB_HEAD_REF,
-    commit: getLatestPRCommit(),
-    build: process.env.GITHUB_RUN_ID,
     isPr,
-    branch,
-    prBranch: isPr ? branch : undefined,
+    commit,
+    build: process.env.GITHUB_RUN_ID,
+    prBranch: process.env.GITHUB_HEAD_REF,
     repository: process.env.GITHUB_REPOSITORY,
     accountId: process.env.GITHUB_REPOSITORY_OWNER_ID,
     repositoryId: process.env.GITHUB_REPOSITORY_ID,
     root: process.env.GITHUB_WORKSPACE,
-    ...(isPr ? getPrEvent() : undefined),
   };
 }
 
